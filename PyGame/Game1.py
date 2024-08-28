eb589a4c71bc1e026ae5aba63663f0aa9ddc3001
manh612147 <139056227+manh612147@users.noreply.github.com>
2024-08-28 15:41:15 +0700

    Update Game1.py

--- PyGame/Game1.py
+++ PyGame/Game1.py
@@ -7,7 +7,7 @@ from pygame import mixer
 # Intialize the pygame
 pygame.init()
 
-# create the screen
+# create the screen cua so game1234
 screen = pygame.display.set_mode((800, 600))
 
 # Background
